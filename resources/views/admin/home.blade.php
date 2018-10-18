77c2ec5b104633e2fafc1225b25b3240dfbf52bd
yudhaprsty <yudha14.yp@gmail.com>
2018-10-18 12:13:43 +0700

    project

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -20,6 +20,13 @@
             <h3>Pasangan Calon</h3>
           </div>
         </div>
+        <div class="animated flipInY col-lg-3 col-md-3 col-sm-6 col-xs-12">
+          <div class="tile-stats">
+            <div class="icon"><i class="fa fa-shopping-cart"></i></div>
+            <div class="count">{{ App\User::where('pilihan', '!=', 'NULL')->get()->count() }}</div>
+            <h3>Jumlah Suara</h3>
+          </div>
+        </div>
       </div>
     </div>
   </div>
